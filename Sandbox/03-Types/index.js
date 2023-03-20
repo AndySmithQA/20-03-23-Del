50af1be21612224aa12fe330e5665aec7ed878a5
AndySmithQA <andy.smith@qa.com>
2023-03-20 13:30:19 +0000

    first commit

---
+++ Sandbox/03-Types/index.js
@@ -0,0 +1,68 @@
+// //myVar = 4; //implicit NEVER EVER DO THIS
+
+// let a = 10; //explicit
+// let y; //explicit without assignemnt
+
+// a = 12.66;
+// a = "12.66";
+// a = 12.45;
+// console.log(a)
+// console.log(y)
+// y = "";
+// console.log(y)
+
+// const z = 11;
+// let _myvar = "Jim"; // _ is usually private
+// let $myNewVar = 5; //compatability + interpolation
+
+// let aVar = 6;
+// let avar = 6;
+
+// let thisIsMyVar = "Dave";
+
+//boolean 
+let myDec =  5 < 3;
+// //myDec = false;
+// console.log(myDec)
+
+// if(myDec) {
+//     console.log("That was true")
+// }else{
+//     console.log("That was not true")
+// }
+//string
+// let myStr = "jkhfjksdfh";
+// if(myStr) {
+//     console.log("That was true")
+// }else{
+//     console.log("That was not true")
+// }
+
+//int / numbers
+
+// let age = 44;
+// age = 66;
+// let price = 0.000000345;
+
+
+// let age2 = null
+// console.log(age2)
+// console.log(typeof age)
+// age = "66";
+// console.log(typeof age)
+// console.log(typeof myDec) // 0 = False 1 = True
+// let j;
+// // console.log(typeof j)
+
+// let loggedOn = Math.sqrt(-37)
+// console.log(loggedOn)
+
+//interpolation
+let a = 5;
+let names = "Bill"
+console.log(`Welcome ${names}, you are ${a} years old `)
+console.log(`5 + 3 is ${5 + 3}`)
+
+console.log(names.indexOf("l"))
+
+console.log(Number.MAX_VALUE)
