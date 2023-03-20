3075906d764c315e3d8c7a728bc19aba81020033
AndySmithQA <andy.smith@qa.com>
2023-03-20 15:01:39 +0000

    Chapt 4

---
+++ Sandbox/04-Operators/index.js
@@ -0,0 +1,111 @@
+//BIDMAS - 
+//BODMAS
+
+//let a = 5;
+
+// a = a + 5;
+// console.log(a)
+
+// a += 5;
+// console.log(a)
+
+// a -= 2;
+// a *= 10;
+// a /= 2;
+// console.log(a)
+
+// a++;
+// console.log(a)
+
+// ++a or a++
+// -9 Unary
+
+//let a = 5;
+// let x = a++;
+// console.log(x, a)
+
+// console.log(5 + 6)
+// console.log(5 * 6)
+// console.log(10 / 2)
+// console.log(10 - 2)
+// let a = 8;
+// console.log(a ** 2)
+//console.log(20 % 4) //Mod operator
+
+// console.log(Math.floor(10/3))
+// console.log(Math.floor(5.9))
+// console.log(Math.pow(4,6))
+
+// a = -a;
+//Brackets Orders Division Multi Add Sub
+//console.log(Math.pow(9, 6))
+
+//Relational Operators
+
+//less than < greater than > 
+//less than or equal to <=
+//greater than or equal to >=
+// == Comparative equals
+// === type comparison
+// let a = 6;
+// if ( a == "6"){
+//     console.log("It was 6")
+// }
+
+// if ( a === "7"){
+//     console.log("the 6 was a string")
+// } else {
+//     console.log("It was 6 but not the same type")
+// }
+
+// if ( a != "6"){
+//     console.log("It was 6")
+// }
+
+//Boolean Operators
+// // Boolean And
+// console.log(2 > 1 && 5 < 4)
+// //Boolean OR
+// console.log(2 > 1 || 5 < 4)
+
+// console.log(!false)
+
+// let a = 2;
+// let b = "two";
+// let c = "2";
+
+// // console.log(typeof a)
+// // console.log(typeof b)
+// // console.log(typeof c)
+
+// console.log(a * a)
+// console.log(typeof (a * a))
+// console.log(a + b)
+// console.log(typeof (a + b))
+// console.log(a * c)
+// console.log(typeof (a * c))
+
+// if (typeof a != b)
+
+// let a = 2;
+// let b = "2";
+// let c = (a !== b)
+// console.log(c)
+
+//Type Conversion
+
+//console.log(eval("4 * 3 + 3"))
+
+//parseInt()
+
+// let z = "6.4";
+// console.log(typeof z)
+// console.log(typeof parseInt(z))
+// z = parseFloat(z)
+// console.log(z)
+
+// console.log(parseInt("55.95bcxnbxcmnbm"))
+
+// let s = 5;
+// console.log(!isNaN(s))
+
