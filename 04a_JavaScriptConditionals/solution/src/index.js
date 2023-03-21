bbccfeb73b8c1b6991a7ded12c01b65b88f8d71e
AndySmithQA <andy.smith@qa.com>
2023-03-21 10:33:17 +0000

    End Session 1

--- 04a_JavaScriptConditionals/solution/src/index.js
+++ 04a_JavaScriptConditionals/solution/src/index.js
@@ -2,18 +2,15 @@ let age = 15;
 
 if(age <= 17) {
     console.log("Underage");
-}
-else {
+} else {
     console.log("18 or over");
 }
 
 if(age <= 17) {
     console.log("Underage")
-}
-else if(age <=65) {
+} else if(age <=65) {
     console.log("Insurable");
-}
-else {
+} else {
     console.log("out of range");
 }
 
