21e63978d7a2c7a8d7b1db070bd4ad61d4c9af26
AndySmithQA <andy.smith@qa.com>
2023-03-21 09:14:39 +0000

    folder renaming

---
+++ Sandbox/04-FlowControl/index.js
@@ -0,0 +1,26 @@
+// let score = prompt("Enter your Score")
+
+// if (score > 100 || score < 0){
+//     console.log("Out of bounds, score is between 0 and 100")
+// } else {
+//     if (score > 90){
+//         console.log(`Your score of ${score} is an A*`)
+//     } else if (score > 80){
+//         console.log(`Your score of ${score} is an A`)
+//     } else if (score < 70){
+//         console.log(`Your score of ${score} is a Fail`)
+//     } 
+// }
+
+let now = new Date();
+// let greeting = "Good";
+
+// if (now.getHours() > 17){
+//     greeting += " evening";
+// } else {
+//     greeting += " day";
+// }
+
+
+// let greeting = "Good" + ((now.getHours() > 17) ? " evening." : " day.")
+// console.log(greeting)
