ff2f331cd7f26a95266ed300e0b7e032f27a1767
AndySmithQA <andy.smith@qa.com>
2023-03-22 10:11:33 +0000

    Day 3 - Session 1

---
+++ Sandbox/08-Maps/index.js
@@ -0,0 +1,48 @@
+//key value pairs
+
+// let myMap = new Map(
+//     [
+//         ["city","London"],
+//         ["population",9000000],
+//         ["size",70]
+//     ]
+// )
+
+// myMap.set("Mayor","SK")
+// console.log(myMap)
+// myMap.delete("Mayor")
+// console.log(myMap)
+// myMap.clear()
+// console.log(myMap)
+// console.log(myMap)
+// console.log(myMap.size)
+
+
+//console.log(myMap.get("city"))
+//console.log(myMap.has("nightlife"))
+
+// myMap.forEach(entry => {
+//     console.log(entry)
+// })
+
+// for (let key of myMap.keys()){
+//     console.log(`key: ${key}`)
+// }
+
+// for (let [key, value] of myMap){
+//     console.log(`${key} - ${value}`)
+// }
+
+
+//Sets
+//each item must be unique
+
+// let mySet = new Set()
+
+// mySet.add("House")
+// mySet.add("Garden")
+// console.log(mySet)
+// mySet.add("House")
+// console.log(mySet)
+// console.log(mySet.has("Driveway"))
+// console.log(mySet.size)
